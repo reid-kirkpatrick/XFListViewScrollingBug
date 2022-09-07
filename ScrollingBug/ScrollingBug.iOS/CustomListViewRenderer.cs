421d5e676142096ee0a4a7d27c8b898494a65b6c
Reid Kirkpatrick <reidkirkpatrick@gmail.com>
2022-09-07 13:03:03 -0500

    Add project files.

---
+++ ScrollingBug/ScrollingBug.iOS/CustomListViewRenderer.cs
@@ -0,0 +1,17 @@
+﻿using ScrollingBug.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ListView), typeof(CustomListViewRenderer))]
+namespace ScrollingBug.iOS
+{
+	public class CustomListViewRenderer : ListViewRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+		{
+			base.OnElementChanged(e);
+
+			MessagingCenter.Instance.Subscribe<MainPage>(this, MainPage.DisableScrollMessage, x => Control.ScrollEnabled = !Control.ScrollEnabled);
+		}
+	}
+}
---
+++ ScrollingBug/ScrollingBug/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ScrollingBug
+{
+	public partial class MainPage : ContentPage
+	{
+		public static string DisableScrollMessage { get; } = "DisableScroll";
+
+		public List<string> Items { get; } = new List<string>();
+
+		public MainPage()
+		{
+			InitItems();
+			InitializeComponent();
+			ToolbarItems.Add(new ToolbarItem("Disable Scroll", null, () =>
+			{
+				MessagingCenter.Send(this, DisableScrollMessage);
+			}));
+		}
+
+		private void InitItems()
+		{
+			var random = new Random();
+			for (int i = 0; i < 50; i++)
+			{
+				var value = $"ListView Scroll Bug Row {i}";
+				var sb = new StringBuilder();
+				for (int j = 0; j < random.Next(10); j++)
+				{
+					sb.AppendLine(value);
+				}
+
+				sb.Append(value);
+				Items.Add(sb.ToString());
+			}
+		}
+	}
+}
